c314078b13f7b15e981fe3d5a852fb8db5d11fa2
Hajime Ito <itorg110@gmail.com>
2020-01-13 04:27:58 +0900

    FlyerViewのアップデートとセッティング用の瀬みっモーダルビューの作成。Flyer作成画面の一部実装。

---
+++ iosclient/PocketSanta/FlyerCreateLocationInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FlyerCreateLocationInfoViewController.swift
+//  PocketSanta
+//
+//  Created by hajime ito on 2020/01/12.
+//  Copyright © 2020 hajime. All rights reserved.
+//
+
+import UIKit
+
+class FlyerCreateLocationInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iosclient/PocketSanta/FlyerSemiModalViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FlyerFloatingPanelViewController.swift
+//  PocketSanta
+//
+//  Created by hajime ito on 2020/01/13.
+//  Copyright © 2020 hajime. All rights reserved.
+//
+
+import Foundation
